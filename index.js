7d18f88e9fd077f7bdf86572e096f6525eeb6e81
Tebbo <leetebbs@ymail.com>
2023-05-01 15:32:01 +0100

    initial upload

---
+++ index.js
@@ -0,0 +1,639 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = 3001;
+const { ethers } = require("ethers");
+const { Network, Alchemy } = require("alchemy-sdk");
+const provider = new ethers.providers.JsonRpcProvider(process.env.REACT_APP_FULL);
+
+const raffleAbi = [
+  {
+    inputs: [],
+    name: "acceptOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_raffleId",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_numberOfTickets",
+        type: "uint256",
+      },
+    ],
+    name: "buyTicket",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_numberOfTickets",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_priceOfTickets",
+        type: "uint256",
+      },
+      {
+        internalType: "address",
+        name: "_nftAddress",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_tokenId",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_raffleLength",
+        type: "uint256",
+      },
+    ],
+    name: "createRaffle",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint64",
+        name: "subscriptionId",
+        type: "uint64",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "have",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "want",
+        type: "address",
+      },
+    ],
+    name: "OnlyCoordinatorCanFulfill",
+    type: "error",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+    ],
+    name: "OwnershipTransferRequested",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+    ],
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "_from",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "_id",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "_tickets",
+        type: "uint256",
+      },
+    ],
+    name: "RaffleCreated",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "requestId",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256[]",
+        name: "randomWords",
+        type: "uint256[]",
+      },
+    ],
+    name: "rawFulfillRandomWords",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "requestId",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256[]",
+        name: "randomWords",
+        type: "uint256[]",
+      },
+    ],
+    name: "RequestFulfilled",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "requestId",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint32",
+        name: "numWords",
+        type: "uint32",
+      },
+    ],
+    name: "RequestSent",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_nftAddress",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_tokenId",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_raffleId",
+        type: "uint256",
+      },
+    ],
+    name: "returnNft",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+    ],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "raffleId",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "address",
+        name: "winner",
+        type: "address",
+      },
+    ],
+    name: "Winner",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "contractOwner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "counter",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "endedRaffleId",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "fee",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_requestId",
+        type: "uint256",
+      },
+    ],
+    name: "getRequestStatus",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "fulfilled",
+        type: "bool",
+      },
+      {
+        internalType: "uint256[]",
+        name: "randomWords",
+        type: "uint256[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "lastRequestId",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "raffle",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "raffleId",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "numberOfTickets",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "priceOfTickets",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "raffleLength",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_initialNumberOfTickets",
+        type: "uint256",
+      },
+      {
+        internalType: "address",
+        name: "nftAddress",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        internalType: "bool",
+        name: "raffleActive",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "raffleTicketHolders",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "requestIds",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "result",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "s_requests",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "fulfilled",
+        type: "bool",
+      },
+      {
+        internalType: "bool",
+        name: "exists",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "winner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "winners",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+const settings = {
+  apiKey: process.env.REACT_APP_API_KEY, // Replace with your Alchemy API Key.
+  network: Network.MATIC_MUMBAI, // Replace with your network.
+};
+
+const alchemy = new Alchemy(settings);
+
+const contractAddress = "0xAA1f4F9386b67eF9cD4CBA5c75a688931C522681"; // mumbai
+
+app.use(cors());
+
+app.get("/tester", cors(), (req, res) => {
+  res.json("Hello World!");
+});
+
+//fetch nft data for user wallet
+
+app.get("/data", async (req, res) => {
+  const owner = req.query.address;
+  const page = req.query.page;
+  try {
+    const result = await alchemy.nft.getNftsForOwner(owner, { pageKey: "" });
+    console.log(result);
+    const non = "You dont have any NFTS";
+    if (!result) {
+      res.json(non);
+    }
+    res.json(result);
+    console.log(result.pageKey);
+  } catch (error) {
+    const non = "You dont have any NFTS";
+    console.log(error);
+    res.json(non);
+  }
+});
+
+app.get("/counter", async (req, res) => {
+  const results = [];
+
+  const raffleContract = new ethers.Contract(
+    contractAddress,
+    raffleAbi,
+    provider
+  );
+  try {
+    console.log("fetching Data");
+    const count = await raffleContract.counter();
+    for (let i = 0; i < parseInt(count); i++) {
+      const transaction = await raffleContract.raffle(i);
+      if (transaction.raffleActive) {
+        const metadata = await alchemy.nft.getNftMetadata(
+          transaction.nftAddress,
+          transaction.tokenId
+        );
+        results.push({ transaction, metadata });
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  res.json(results);
+});
+
+app.get("/winners", async (req, res) => {
+  const results = [];
+
+  const raffleContract = new ethers.Contract(
+    contractAddress,
+    raffleAbi,
+    provider
+  );
+  try {
+    const count = await raffleContract.counter();
+    for (let i = 0; i < parseInt(count); i++) {
+      const transaction = await raffleContract.raffle(i);
+      if (!transaction.raffleActive) {
+        const metadata = await alchemy.nft.getNftMetadata(
+          transaction.nftAddress,
+          transaction.tokenId
+        );
+        const winner = await raffleContract.winners(transaction.raffleId);
+        console.log("raffle winner", winner);
+        results.push({ transaction, metadata, winner });
+      }
+    }
+    console.log(results);
+  } catch (error) {
+    console.log(error);
+  }
+  res.json(results);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
