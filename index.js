279930a5022a68b71f12da8345e54e67a6bd15b4
Tebbo <leetebbs@ymail.com>
2023-05-28 18:24:04 +0100

    Update index.js
    add search to smart contract update address

--- index.js
+++ index.js
@@ -628,9 +628,9 @@ const settings = {
 
 const alchemy = new Alchemy(settings);
 
-const contractAddress = "0xAA1f4F9386b67eF9cD4CBA5c75a688931C522681"; // mumbai
+// const contractAddress = "0xAA1f4F9386b67eF9cD4CBA5c75a688931C522681"; // mumbai original 
 // const contractAddress = "0x7b674c826FF2B445C62194759088C8e52f83F71b"; // mumbai
-// const contractAddress = "0x46Eee6B2D16B7C1195Af8C952DfD3AE3ae7DA15B" 
+const contractAddress = "0x172990f74120d3d7F69Cf79f4A3eE4bbcd1015A0" // added search
 
 app.use(cors());
 
